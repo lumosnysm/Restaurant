2ab696f450fb9916efbde922b85b98c357ec9adc
TheNguyen0905 <thebay0905@gmail.com>
2018-08-12 11:24:10 +0700

    Finished sign-up user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,22 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:info] = t "users.create.wellcome"
+      redirect_to root_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password,
+      :password_confirmation
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "/about", to: "static_pages#about"
   get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
   get "login", to: "sessions#new"
+  resources :users
 end
