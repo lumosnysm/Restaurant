8d77ebee2dc406733f9001ec9d25dfa2a05cee88
LongVH <lumosnysm@gmail.com>
2018-08-16 16:34:55 +0700

    Finished review restaurant

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  has_many :reviews
   before_save {email.downcase!}
-  validates :name,  presence: true, length: {maximum: Settings.name_max_length}
+  validates :name, presence: true, length: {maximum: Settings.name_max_length}
   validates :email, presence: true, length: {maximum: Settings.email_max_length},
     format: {with: Settings.valid_email_regex}, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: Settings.pass_min_length},
