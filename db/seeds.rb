47477b42539bc46f920a08f6e4b52df518014a8b
LongVH <lumosnysm@gmail.com>
2018-08-16 16:34:55 +0700

    add admin to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
     price: price,
     description: description)
 end
+
+User.create! name: "Admin",
+  email: "admin@res.com",
+  password: "111111",
+  password_confirmation: "111111",
+  admin: true
