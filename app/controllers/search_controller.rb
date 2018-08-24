e5fb1d3fe0b0c8d5dc3bdcc14a0d40d8a5c13114
LongVH <lumosnysm@gmail.com>
2018-08-24 13:06:21 +0700

    search fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   def index
+    @order_item = current_order.order_items.new
     @dishes = Dish.search_by_name params[:sub_keywords]
     if params[:keywords_search]
       @dishes = Dish.search_by_name params[:keywords_search]
